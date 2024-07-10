a2987393e8abd81167ce3999dbd9955bd81d3114
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-07-10 09:49:18 +0300

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  function fuzzyMatch(drivers, prefix) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(prefix.toLowerCase()));
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+
+  module.exports = { findMatching, fuzzyMatch, matchName };
